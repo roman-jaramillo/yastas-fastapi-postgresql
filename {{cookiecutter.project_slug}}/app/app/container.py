9b2319403cbbc775ce13cc15d7f4fff4eca98388
roman-jaramillo <roman.jaramillo@globant.com>
2023-06-22 13:13:59 -0600

    Example of dependency injector added to items.py

--- {{cookiecutter.project_slug}}/app/app/container.py
+++ {{cookiecutter.project_slug}}/app/app/container.py
@@ -1,8 +1,11 @@
 from dependency_injector import containers, providers
-
+from app.core.config import settings
+from app.api.deps import get_db
 
 class Container(containers.DeclarativeContainer):
     wiring_config = containers.WiringConfiguration(modules=["./api/api_v1/endpoints"])
-    #Provide configuration using "config = providers.Configuration()"
+    
+    # Line to provide configuration config = providers.Configuration(config)
 
     #Add the dependencies here
+    get_db = providers.Factory(get_db)
